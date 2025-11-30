22f9e82b40f7dcd5023a2a8e024837ab56e24496
Bruce Li <isanoob1897@outlook.com>
2025-11-29 21:22:27 -0800

    Initial commit

---
+++ src/main/java/dev/desktop/quizgame/QuizGame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package dev.desktop.quizgame;
+
+/**
+ *
+ * @author bruceli
+ */
+public class QuizGame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
