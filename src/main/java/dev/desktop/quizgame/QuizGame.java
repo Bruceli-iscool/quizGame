dfa4ac60d32ded10043e438bd7fa068b672afe6e
Bruce Li <isanoob1897@outlook.com>
2025-11-29 21:59:25 -0800

    add

--- src/main/java/dev/desktop/quizgame/QuizGame.java
+++ src/main/java/dev/desktop/quizgame/QuizGame.java
@@ -1,16 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package dev.desktop.quizgame;
-
+import java.util.*;
 /**
  *
  * @author bruceli
  */
 public class QuizGame {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String args[]) {
+        mainMenu();
+    }
+    public static void mainMenu() {
+        ArrayList<String> messages = new ArrayList<>();
+        messages.add("1. Single Player");
+        messages.add("2. HotSeat");
+        messages.add("3. Quit");
+        System.out.println("###### QuizGame ######");
+        int counter = 0;
+        String fill = " ";
+        while (counter <3) {
+            System.out.println("# "+messages.get(counter)+fill.repeat(("###### QuizGame ######".length()-("# "+messages.get(counter)).length()-1))+"#");
+            counter++;
+        }
+        System.out.println("######################\nChoose your gamemode:");
+        boolean needed = true;
+        Scanner s = new Scanner(System.in);
+        while (needed) {
+            System.out.print(">");
+            String ans = s.nextLine();
+            if (ans.replace(" ", "").matches("1")) {
+                singleplayer();
+                needed = false;
+            }
+            // todo
+        }
+    }
+    public static void singleplayer() {
+        // todo
     }
 }
